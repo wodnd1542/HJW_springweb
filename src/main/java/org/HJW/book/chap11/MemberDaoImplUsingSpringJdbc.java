139f7840f3bf55b02f61e4e53d492a826112ad02
HJW-jdbc <wodnd1542@naver.com>
2019-05-10 16:44:36 +0900

    5월10일수업

---
+++ src/main/java/org/HJW/book/chap11/MemberDaoImplUsingSpringJdbc.java
@@ -0,0 +1,58 @@
+package org.HJW.book.chap11;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository("memberDao")
+public class MemberDaoImplUsingSpringJdbc implements MemberDao{
+	
+	static final String INSERT = "INSERT member(email, password, name) VALUES(?, sha2(?,256), ?)";
+
+	static final String SELECT_ALL = "SELECT memberId, email, name, left(cdate,19) cdate FROM member ORDER BY memberId desc LIMIT ?,?";
+
+	static final String COUNT_ALL = "SELECT count(memberId) count FROM member";
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	final RowMapper<Member> memberRowMapper = new BeanPropertyRowMapper<>(Member.class);
+
+	@Override
+	public Member selectByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Member member) {
+		jdbcTemplate.update(INSERT, member.getEmail(), member.getPassword(),
+				member.getName());
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Member member) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Member> selectAll(int offset, int count) {
+		// TODO Auto-generated method stub
+		return jdbcTemplate.query(SELECT_ALL, memberRowMapper, offset, count);
+	}
+
+	@Override
+	public int countAll() {
+		// TODO Auto-generated method stub
+		return jdbcTemplate.queryForObject(COUNT_ALL, Integer.class);
+	}
+	
+	
+}
